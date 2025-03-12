994b69486ab29001e9207eb32ce5064f3946137c
Drwexs <146848206+Drwexs@users.noreply.github.com>
2025-03-12 16:35:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
@@ -15,28 +15,26 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuaFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById('alterna-contraste');
 
     let tamanhoAtualFonte = 1;
 
-    aumentaFonteBotao.addEventListener('click' , function(){
+    aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
 
     })
 
-    diminuaFonteBotao.addEventListener('click' , function(){
+    diminuaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
 
     })
 
-
-
-
-
-
-
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
 
 })
